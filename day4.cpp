164c91a8b6a9168a53e00210cbfcd6dc33f064f9
bhumi458 <singhalbhumi02@gmail.com>
2026-05-05 21:52:27 +0530

    commit

--- day4.cpp
+++ day4.cpp
@@ -9,10 +9,107 @@ using namespace std;
 //****
 //****  
 
+// int main(){
+//     for(int i=1;i<=5; i++){
+//         for(int j=1; j<=4; j++){
+//             cout<<"*";
+//         }
+//         cout<<endl;
+//     }
+
+//     return 0;
+// }
+
+
+
+
+//ques-2
+//*
+//**
+//***
+//****  
+
+// int main(){
+//     int r;
+//     cin>>r;
+
+//     for(int i=1; i<=r; i++){
+//         for(int j=1; j<=i; j++){
+//             cout<<"*";
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// }
+
+
+ 
+//ques-3
+
+//   *
+//  **
+// ***
+//****
+//  int main(){
+//    int n;
+//     cin>>n;
+ 
+//     for(int i=1; i<=n; i++){
+//         for(int j=1; j<=(n-i); j++){
+//             cout<<" ";
+//         }
+//         for(int k=1; k<=i; k++){
+//             cout<<"*";
+//         }
+
+//         cout<<endl;
+//     }
+
+//     return 0;
+// }
+
+
+
+
+//ques-4
+//*****
+//*   *
+//*   *
+//*   *
+//*****
+
+// int main(){
+//    int n;
+//    cin>>n;
+//   for(int i=1; i<=n; i++){
+//     for(int j=1; j<=n; j++){
+//         if(i==1 || i==n || j==1 || j==n){
+//             cout<<"*";
+//         }
+//         else{
+//             cout<<" ";
+//         }
+//     }
+//     cout<<endl;
+//   }
+ 
+//   return 0;
+// }
+
+
+
+//pyramid question
+
 int main(){
-    for(int i=1;i<=5; i++){
-        for(int j=1; j<=4; j++){
-            cout<<"*";
+    int n;
+    cin>>n;
+    for(int i=1; i<=n; i++){
+        for(int j=n; j>i; j--){
+            cout<<" ";
+        }
+
+        for(int k=1; k<=i; k++){
+            cout<<" *";
         }
         cout<<endl;
     }
@@ -22,7 +119,44 @@ int main(){
 
 
 
-//ques-2
 
+
+
+//extra 
+//  int main(){
+//    int n;
+//     cin>>n;
+ 
+//     for(int i=1; i<=n; i++){
+//         for(int j=1; j<=(n-i); j++){
+//             cout<<" ";
+//         }
+//         for(int k=1; k<=i; k++){
+//             cout<<i;
+//         }
+
+//         cout<<endl;
+//     }
+
+//         for(int i=n; i>=1; i--){
+//         for(int j=1; j<=(n-i); j++){
+//             cout<<" ";
+//         }
+//         for(int k=1; k<=i; k++){
+//             cout<<i;
+//         }
+
+//         cout<<endl;
+//     }
+
+//     return 0;
+// }
+
+ 
+
+
+
+
+ 
 
 
---
+++ day5.cpp
@@ -0,0 +1,169 @@
+#include<iostream>
+using namespace std;
+
+//functions
+
+//without argument , without return type
+// void printmsg(){
+//     cout<<"hello world";
+// }
+
+// int main(){
+//     printmsg();
+//     return 0;
+// }
+
+
+//with argument without return type
+
+// void add(int a , int b){
+//     cout<<a+b<<endl;
+// }
+
+// int main(){
+//     add(2 , 3);
+//     return 0;
+// }
+
+
+//with argument , with return type
+
+// int sum(int a , int b){
+//     int add = a + b;
+//     cout<<add<<endl;
+//     return 0;
+// }
+
+// int main(){
+//     sum(6 , 7);
+//     return 0;
+// }
+
+
+
+//without argument , with return type
+
+// int printmsg(){
+//     cout<<"hello bhumi"<<endl;
+//     return 0;
+// }
+
+// int main(){
+//     printmsg();
+//     return 0;
+// }
+
+
+
+
+//pointers
+
+// int main(){
+//     int a = 10;
+//     int *ptr = &a;
+
+//     cout<<ptr<<endl;
+//     return 0;
+// }
+
+
+// 2nd-code pointer
+
+// int main(){
+//     int value = 10;
+//     int *ptr = &value;
+
+//     cout<<"ADD;"<<ptr<<endl;
+//     cout<<"value = "<<*ptr<<endl;
+
+//     *ptr = 20;
+//     cout<<"value = "<<*ptr<<endl;
+//     cout<<"value = "<<value<<endl;
+
+//     return 0;
+// }
+
+
+
+
+//WAP that takes coffee sort return the total number of coffee volume if 1sort equal to 30ml 
+ 
+//  int coffeesort(int sort){
+//     int total = sort  * 30;
+//     cout<<"total coffee volume = "<<total<<"ml"<<endl;
+//     return 0; 
+//  }
+
+// int main()
+// {
+//     int sort;
+//     cout<<"enter the number of coffee short:"<<endl;
+//     cin>>sort;
+
+//     coffeesort(sort);
+//     return 0;
+// }
+
+
+
+
+//write a function with default argument and calling it with ir without parameters 
+
+// int add(int a = 5 , int b = 5){
+//     cout<<"sum = "<<a +b<<endl;
+//     return 0;
+// }
+
+// int main(){
+
+//     add();
+//     add(10 , 10);
+//     return 0;
+// }
+
+
+//WAP that apply 10% discount using references update the original bill amount and print the original bill
+
+// int discountAtbill(float &bill){
+//        bill = bill * 0.9;
+//        cout<<"bill after discount = "<<bill<<endl;
+//        return 0;
+// }
+
+// int main(){
+
+//     float bill;
+//     cout<<"enter the bill amount:"<<endl;
+//     cin>>bill;
+
+//     discountAtbill(bill);
+//     return 0;
+// }
+
+
+// swap two number using pointer
+
+int swap(int &a , int &b){
+    int *ptr1 = &a;
+    int *ptr2 = &b;
+
+    int temp;
+
+     temp = *ptr1,
+    *ptr1 = *ptr2,
+    *ptr2 = temp;
+
+    cout<<"after swapping a = "<<a <<"and b="<<b<<endl;
+    
+    return 0;
+}
+
+int main(){
+    int a , b;
+    cout<<"enter the value of a and b"<<endl;
+    cin>>a >> b;
+
+    swap(a , b);
+    return 0;
+
+}
